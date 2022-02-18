99ec03863cf890e48de9ed70a339d52ce8e013ab
nishi <nisiwaki@shinwa-made.co.jp>
2022-02-18 13:21:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Common/IStructureSteelBeam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeSteelLib.Common
+{
+    /// <summary>
+    /// 棒鋼のインターフェイスを記述
+    /// </summary>
+    public interface IStructureSteelBeam
+    {
+        /// <summary>
+        /// 鋼材長を記録する関数、mmで記録しておいた方がよさそうである。
+        /// </summary>
+        double length { get; set; }
+        /// <summary>
+        /// 鋼材表面積をcm2で返す関数、ジェネリック定義で十分ではある。
+        /// </summary>
+        /// <returns>鋼材表面積(cm2)を返す</returns>
+        double surfaceArea();
+        /// <summary>
+        /// 鋼材重量をkgで返す関数、基本はジェネリックで定義しておく
+        /// </summary>
+        /// <returns>鋼材重量(kg)を返す<</returns>
+        double weight();
+    }
+}
---
+++ Common/IstructureSteelCrossSection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ShapeSteelLib.Common
+
+{
+    /// <summary>
+    /// 棒鋼のインターフェイスを記述
+    /// </summary>
+    public interface IStructureSteelCrossSection
+    {
+        /// <summary>
+        /// 鋼材断面積(cm2)を計算し返す関数
+        /// </summary>
+        /// <returns>鋼材断面積(cm2)</returns>
+        double crossSectionArea();
+        /// <summary>
+        /// 鋼材表面長(cm)を計算し返す関数
+        /// </summary>
+        /// <returns>鋼材周長(cm)</returns>
+        double crossSectionRerimeter();
+    }
+}
